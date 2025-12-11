5cd990d25013053566c541a1ad8c16443a0eca8b
Rachid Echafai <rachidchaf2001@gmail.com>
2025-12-11 12:33:12 +0100

    chore: add prettier formatting to lint-staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,14 +1,15 @@
 // npx tsc --noEmit && npx eslint --fix . && npx prettier --write .
 module.exports = {
-  // this will check Typescript files
+  // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'npx tsc --noEmit',
 
-  // This will lint and format TypeScript and JavaScript files
+  // Lint and format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
     `npx eslint --fix ${filenames.join(' ')}`,
+    `npx prettier --write ${filenames.join(' ')}`,
   ],
 
-  // this will Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) =>
+  // Format MarkDown, JSON and CSS
+  '**/*.(md|json|css)': (filenames) =>
     `npx prettier --write ${filenames.join(' ')}`,
 }
